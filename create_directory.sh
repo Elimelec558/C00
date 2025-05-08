cb48789f4050d1c796a27c03d0bd73ad43fc1fcc
Elimelec-Emmanuel TOGUYENI <tgnemmanuel@gmail.com>
2025-05-08 15:18:18 +0000

    Ajout de tous les fichiers concernant C00

---
+++ create_directory.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script permettant de créé repertoir de nom "text_director"
+pwd
+mkdir text_directory
+cd text_directory
+echo "Alors,Mon Nouveau Repertoir à très été créé"
---
+++ list_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Ecriture d'un script affichant tout les fichiers du repertoire courant.
+#!/bin/bash
+#!/bin/bash
+
+# Affichage d'un message avec le chemin du répertoire courant
+c/Users/Lenovo/documents/codeloccol.
+# Liste tous les fichiers et dossiers du répertoire courant
+ls -l
+
